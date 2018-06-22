67aabbe8d3ad195b85d0ba44bd9b1c7c2a978fe8
tommyjakobsen <tommy.jakobsen@outlook.com>
2018-06-22 13:51:47 +0200

    added setup and db sql class

---
+++ classes/classSql.php
@@ -0,0 +1,46 @@
+<?php
+
+error_reporting(E_ALL);
+/**
+  * mySql Class
+  * 
+  * Class for running simple sql's against transition database. Used db connection config in pg_connect.php
+  *
+  * @param string $sql will return array in array with result of sql, where key values are table field names
+  *    span multiple lines.
+  *
+  * @return array
+  */
+class mySql{ 
+
+        function query($sql){
+				//GET home folder
+			$homefolder=getenv('APP_ROOT);
+                $sapi_type = php_sapi_name();
+
+                if(preg_match('/cli/', $sapi_type))
+                        {
+                        exit(1);
+                        }else{
+                        include "$homefolder/db/db_connect.php";
+                        }
+
+	
+		
+
+        
+       if ($result = $mysqli->query("$sql")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+            return $result;
+
+            }
+
+        }
+
+
+
+
+}
---
+++ setup.php
@@ -0,0 +1,29 @@
+<?php
+
+$dbhost=getenv('MARIADB_SERVICE_HOST');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
+$database=getenv("MYSQL_DATABASE");
+if($dbhost == "")
+        {
+
+        echo "<font color=red> No DB or environment variables created in this openshift project....</font>";
+         echo "Need:<br><b>MYSQL_DB_HOST<\b><br><b>MYSQL_USERNAME<\b><br><b>MYSQL_PASSWORD<\b><br><b>MYSQL_DATABASE<\b>";
+        exit(1);
+        }
+
+$link = mysqli_connect("$dbhost", "$username", "$password", "$database");
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+//DEBUG:
+//echo "Success: A proper connection to MySQL was made! The $database database is great." . PHP_EOL;
+//echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+
+?>
