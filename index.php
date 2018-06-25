7d233afd8db7a897ca6b3eb4c8bab55db06e096a
tommyjakobsen <tommy.jakobsen@outlook.com>
2018-06-25 13:18:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,55 @@ if(!file_exists('./setup'))
   //Go to setup page....
   echo "<meta http-equiv=\"refresh\" content=\"5;url=/setup.php\" />";
 }else{
+ 
   //Show the statistics
-  
+echo "<!DOCTYPE HTML>
+<html>
+<head>
+  <script type=\"text/javascript\">
+  window.onload = function () {
+    var chart = new CanvasJS.Chart(\"chartContainer\", {
+
+      title:{
+        text: \"Top 10 Populated Countries in the World\"
+      },
+      data: [//array of dataSeries
+        { //dataSeries object
+
+         /*** Change type \"column\" to \"bar\", \"area\", \"line\" or \"pie\"***/
+         type: \"column\",
+         dataPoints: [
+        ";
+        $rowcount=0;
+        while($row = $result->fetch_assoc()) {
+        $rowcount++;
+        if($rowcount < 10)
+                {
+                echo "{ label: \"$row[Name]\", y: $row[Population] },\n";
+                }else{
+                echo "{ label: \"$row[Name]\", y: $row[Population] }\n";
+
+                }
+
+        }
+
+
+
+echo "        ]
+       }
+       ]
+     });
+
+    chart.render();
+  }
+  </script>
+  <script type=\"text/javascript\" src=\"https://canvasjs.com/assets/script/canvasjs.min.js\"></script>
+</head>
+<body>
+  <div id=\"chartContainer\" style=\"height: 300px; width: 100%;\">
+  </div>
+</body>
+</html>";
 }
 
 
