e48429dedf72802f4b1b78f076b0da67281691ed
tommyjakobsen <tommy.jakobsen@outlook.com>
2018-06-25 09:46:57 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -49,7 +49,25 @@ $newSql=new mySql();
 
 //Removing the constraints, since they don't work in mariadb (have not had time to fix them yet)
 $sql=preg_replace('/(,\n.*CONSTRAINT.*)|(\n.*world.*)|(--.*)|(\n\/\*.*)/i', '', file_get_contents("$out_file_name"));
-$result=$newSql->query("$sql");
 
+//Since the sql get's to big, I need to split it up to chuncks
+//Running one sql pr. creation of table and insert into that table
+$sql_part="";
+$part_count=0;
+foreach(explode("\n", $sql) as $line) {
+    if(preg_match('/CREATE TABLE/i', $line))
+        {
+        if($part_count > 0)
+                {
+                echo "$sql_part\n\n--------------------------------------------\n\n";
+                $result=$newSql->query("$sql_part");
+                }
+        echo "\n$line\n";
+        $part_count++;
+        $sql_part="$line\n";
+        }else{
+        $sql_part.="$line\n";
+        }
+}
 print_r($result);
 ?>
